bcc70016424a1a6cf7813b31d0758f86848917cb
ram <ramks@thoughtworks.com>
2019-03-13 23:17:44 +0530

    Add logout, fixed login button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./header.module.css";
+import authService from "../../services/authService";
 
 const Header = props => {
+  const logout = () => {
+    authService.unset();
+    window.location.href = "/";
+  };
+
   const el = props.isAuthenticated ? (
-    <Link className={styles.link} to="/">
-      Discover Books
-    </Link>
+    <div>
+      <Link className={styles.link} to="/">
+        Discover Books
+      </Link>
+      <img
+        className={styles.logout}
+        onClick={logout}
+        src="../logout.png"
+        alt="logout"
+      />
+    </div>
   ) : (
     <Link className={styles.link} to="/login">
       Login
